47f59af9d0c0ddc0846bde31a9caf419ac3ba86c
Neil Eichelberger <neileich@live.com>
2018-04-15 13:20:28 -0500

    Add project files.

---
+++ exercise01_array01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercise01_array01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
